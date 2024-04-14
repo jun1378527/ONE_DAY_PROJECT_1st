e6a4b13ee87faa0e8f3fb7ce36ea4bd9bb335c9f
jun1378527 <81806934+jun1378527@users.noreply.github.com>
2024-04-14 21:57:17 +0900

    js modify

--- js/main.js
+++ js/main.js
@@ -27,20 +27,17 @@ setInterval(() => {
   btns[selected].classList.add("active");
 }, 7000);
 
-// 로그인 버튼 클릭 이벤트
 document
   .getElementById("loginButton")
   .addEventListener("click", function (event) {
-    event.preventDefault(); // 기본 이벤트를 방지합니다.
-    document.getElementById("loginModal").style.display = "block"; // 로그인 모달을 표시합니다.
+    event.preventDefault();
+    document.getElementById("loginModal").style.display = "block";
   });
 
-// 닫기 버튼 클릭 이벤트
 document.querySelector(".close").addEventListener("click", function () {
-  document.getElementById("loginModal").style.display = "none"; // 로그인 모달 숨김
+  document.getElementById("loginModal").style.display = "none";
 });
 
-// 윈도우 밖 클릭 시 로그인 창 닫기
 window.onclick = function (event) {
   if (event.target == document.getElementById("loginModal")) {
     document.getElementById("loginModal").style.display = "none";
