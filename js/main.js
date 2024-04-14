216361012697be6f3c9f3e0a1669c248c1e86d24
jun1378527 <81806934+jun1378527@users.noreply.github.com>
2024-04-14 21:51:34 +0900

    login add

--- js/main.js
+++ js/main.js
@@ -25,4 +25,24 @@ setInterval(() => {
   carousel.style.backgroundImage = `url(${pics[selected]})`;
   banner.className = "banner " + gradients[selected];
   btns[selected].classList.add("active");
-}, 5000);
+}, 7000);
+
+// 로그인 버튼 클릭 이벤트
+document
+  .getElementById("loginButton")
+  .addEventListener("click", function (event) {
+    event.preventDefault(); // 기본 이벤트를 방지합니다.
+    document.getElementById("loginModal").style.display = "block"; // 로그인 모달을 표시합니다.
+  });
+
+// 닫기 버튼 클릭 이벤트
+document.querySelector(".close").addEventListener("click", function () {
+  document.getElementById("loginModal").style.display = "none"; // 로그인 모달 숨김
+});
+
+// 윈도우 밖 클릭 시 로그인 창 닫기
+window.onclick = function (event) {
+  if (event.target == document.getElementById("loginModal")) {
+    document.getElementById("loginModal").style.display = "none";
+  }
+};
