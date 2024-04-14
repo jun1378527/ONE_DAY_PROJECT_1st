32f6b3853ef0ea2a442ddf7ea3eb6ebd121dffad
jun1378527 <81806934+jun1378527@users.noreply.github.com>
2024-04-14 15:56:44 +0900

    carousel add and modify

--- js/main.js
+++ js/main.js
@@ -1,39 +1,31 @@
 const carousel = document.querySelector(".carousel");
-       
+const btns = document.querySelectorAll(".btn");
 const pics = [
-    "img/supreme_margiela_T-shirts1.png",
-    "img/supreme_margiela_T-shirts2.png",
-    "img/chromehearts_lgmambk.jpg",
-    "img/palace_gap_jacket1.png",
-    "img/gentlemonster_margiela_sunglass1.png",
-    "img/gentlemonster_margiela_sunglass2.png",
-    "img/nike_sb_dunk_low_chicago.png",
-    "img/CHANEL_ALLURE.jpg"
-]
-
+  "img/supreme_margiela_T-shirts1.png",
+  "img/supreme_margiela_T-shirts2.png",
+  "img/chromehearts_lgmambk.jpg",
+  "img/palace_gap_jacket1.png",
+  "img/gentlemonster_margiela_sunglass1.png",
+  "img/gentlemonster_margiela_sunglass2.png",
+];
 
 // carousel의 초기 이미지와 버튼색 초기값 설정
 let selected = 0;
 
 carousel.style.backgroundImage = `url(${pics[selected]})`;
-carousel.style.backgroundRepeat = "no-repeat";
 
-const btns = document.querySelectorAll(".btn");
-btns[selected].style.backgroundColor = "black";
-
-for (let i = 0 ; i < pics.length; i++) {
-    btns[i].onclick = () => {
-        btns[selected].style.backgroundColor = "white";
-        carousel.style.backgroundImage = `url(${pics[i]})`;
-        btns[i].style.backgroundColor = "black";
-        selected = i;  
-    }
-}
+btns.forEach((btn, index) => {
+  btn.onclick = function () {
+    carousel.style.backgroundImage = `url(${pics[index]})`;
+    btns[selected].classList.remove("active");
+    btn.classList.add("active");
+    selected = index;
+  };
+});
 
 setInterval(() => {
-    btns[selected].style.backgroundColor = "white"; // 값을 증가시키기 전에 이전에 활성화되었던 버튼색을 초기화
-    selected++; 
-    if(selected >= pics.length) selected = 0; // 현재 증가된 값이 pics의 length보다 크다면 다시 0으로 초기화
-    carousel.style.backgroundImage = `url(${pics[selected]})`;
-    btns[selected].style.backgroundColor = "black"; 
-}, 5000); 
+  btns[selected].classList.remove("active");
+  selected = (selected + 1) % pics.length;
+  carousel.style.backgroundImage = `url(${pics[selected]})`;
+  btns[selected].classList.add("active");
+}, 5000);
